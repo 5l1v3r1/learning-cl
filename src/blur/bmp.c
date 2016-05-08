77b42ea5444e2def5615303ccfe35889ea2d792f
Alex Nichol <unixpickle@gmail.com>
2016-05-07 22:43:03 -0400

    support BMPs with alpha channels

--- src/blur/bmp.c
+++ src/blur/bmp.c
@@ -23,6 +23,8 @@ typedef struct {
   uint32_t importantColors;
 } __attribute__((packed)) bmp_header_t;
 
+static uint8_t * read_padded_rgb_data(FILE * fp, bmp_header_t head);
+
 bmp_t * bmp_read(const char * path) {
   assert(sizeof(cl_uchar4) == 4);
 
@@ -37,7 +39,7 @@ bmp_t * bmp_read(const char * path) {
     return NULL;
   }
 
-  if (head.bitsPerPixel != 24) {
+  if (head.bitsPerPixel != 24 && head.bitsPerPixel != 32) {
     fclose(fp);
     return NULL;
   }
@@ -47,52 +49,35 @@ bmp_t * bmp_read(const char * path) {
     return NULL;
   }
 
-  size_t pixelCount = (size_t)(head.width * head.height);
-  size_t dataSize = pixelCount * 3;
-  uint8_t * data = (uint8_t *)malloc(dataSize);
+  uint8_t * pixelData;
+  if (head.bitsPerPixel == 24) {
+    pixelData = read_padded_rgb_data(fp, head);
+  } else {
+    size_t dataSize = 4 * head.width * head.height;
+    pixelData = (uint8_t *)malloc(dataSize);
+    if (pixelData) {
+      if (fread(pixelData, 1, dataSize, fp) != dataSize) {
+        free(pixelData);
+        pixelData = NULL;
+      }
+    }
+  }
 
-  if (!data) {
+  if (!pixelData) {
     fclose(fp);
     return NULL;
   }
-
-  if (fread(data, 1, dataSize, fp) != dataSize) {
-    fclose(fp);
-    free(data);
-    return NULL;
-  }
-  fclose(fp);
-
-  uint8_t * paddedBuff = (uint8_t *)malloc(pixelCount * 4);
-
-  if (!paddedBuff) {
-    free(data);
-    fclose(fp);
-    return NULL;
-  }
-
-  size_t i;
-  for (i = 0; i < pixelCount; i++) {
-    size_t sourceIdx = i * 3;
-    size_t destIdx = i * 4;
-    paddedBuff[destIdx++] = data[sourceIdx++];
-    paddedBuff[destIdx++] = data[sourceIdx++];
-    paddedBuff[destIdx++] = data[sourceIdx++];
-    paddedBuff[destIdx] = 0;
-  }
-
-  free(data);
 
   bmp_t * res = (bmp_t *)malloc(sizeof(bmp_t));
 
   if (!res) {
-    free(paddedBuff);
+    free(pixelData);
     return NULL;
   }
 
   res->width = (int)head.width;
   res->height = (int)head.height;
-  res->pixels = (cl_uchar4 *)paddedBuff;
+  res->pixels = (cl_uchar4 *)pixelData;
 
   return res;
 }
@@ -152,4 +137,40 @@ int bmp_write(bmp_t * b, const char * path) {
 void bmp_free(bmp_t * b) {
   free(b->pixels);
   free(b);
+}
+
+static uint8_t * read_padded_rgb_data(FILE * fp, bmp_header_t head) {
+  size_t pixelCount = (size_t)(head.width * head.height);
+  size_t dataSize = pixelCount * 3;
+  uint8_t * data = (uint8_t *)malloc(dataSize);
+
+  if (!data) {
+    return NULL;
+  }
+
+  if (fread(data, 1, dataSize, fp) != dataSize) {
+    free(data);
+    return NULL;
+  }
+  fclose(fp);
+
+  uint8_t * paddedBuff = (uint8_t *)malloc(pixelCount * 4);
+
+  if (!paddedBuff) {
+    free(data);
+    return NULL;
+  }
+
+  size_t i;
+  for (i = 0; i < pixelCount; i++) {
+    size_t sourceIdx = i * 3;
+    size_t destIdx = i * 4;
+    paddedBuff[destIdx++] = data[sourceIdx++];
+    paddedBuff[destIdx++] = data[sourceIdx++];
+    paddedBuff[destIdx++] = data[sourceIdx++];
+    paddedBuff[destIdx] = 0;
+  }
+
+  free(data);
+  return paddedBuff;
 }
